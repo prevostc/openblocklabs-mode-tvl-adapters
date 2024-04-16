b1453ec310e4a8b912718ca04c21076fcb64b576
Jhon Lucas <jhonlucaslife@gmail.com>
2024-04-16 14:01:36 -0300

    chore: remove unused files.

--- adapters/tvl-snapshot-airpuff/dist/constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOKEN = exports.addresses = void 0;
-exports.addresses = {
-    ezETH: '0x2416092f143378750bb29b79ed961ab195cceea5',
-    usdt: '0xf0f161fda2712db8b566946122a5af183995e2ed',
-    eth: '0x'
-};
-exports.TOKEN = ['ezETH', 'usdt', 'eth'];
