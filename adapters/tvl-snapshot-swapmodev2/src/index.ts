1a1f865419b44853e044f52478e587b6012dc954
Jhon Lucas <jhonlucaslife@gmail.com>
2024-04-23 19:05:03 -0300

    fix: change adapter logic.

--- adapters/tvl-snapshot-swapmodev2/src/index.ts
+++ adapters/tvl-snapshot-swapmodev2/src/index.ts
@@ -1,7 +1,5 @@
 import { GraphQLClient, gql } from "graphql-request";
 import { BigNumber } from "bignumber.js";
-import { join } from "path";
-import { commify } from "ethers/lib/utils";
 import { ethers } from "ethers";
 import fs from "fs";
 import * as path from "path";
@@ -10,115 +8,293 @@ import csv from 'csv-parser';
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const modeV2Client = new GraphQLClient(
-  "https://api.goldsky.com/api/public/project_cltceeuudv1ij01x7ekxhfl46/subgraphs/swapmode-v2/1.0.4/gn"
+  "https://api.goldsky.com/api/public/project_cltceeuudv1ij01x7ekxhfl46/subgraphs/swapmode-v2/prod/gn"
+);
+const modeV3Client = new GraphQLClient(
+  "https://api.goldsky.com/api/public/project_cltceeuudv1ij01x7ekxhfl46/subgraphs/swapmode-v3/prod/gn"
 );
 const modeNFTPoolsClient = new GraphQLClient(
   "https://api.goldsky.com/api/public/project_cltceeuudv1ij01x7ekxhfl46/subgraphs/nft-pools-mode/prod/gn"
 );
+
 const provider = new ethers.providers.JsonRpcProvider("https://mainnet.mode.network/");
+const BN_ZERO = new BigNumber(0);
+// TODO: add V3
 
-async function getBlockData(block: number) {
-  try {
-    // To filter pool addresses out of snapshot list
-    const pools: any = await modeNFTPoolsClient.request(gql`
-      query GetNFTPools {
-        nftpools {
+const pools: {
+  [pool: string]: {
+    lpPrice: BigNumber;
+    reserveUSD: BigNumber;
+    totalSupply: BigNumber;
+  };
+} = {};
+let nftPoolAddresses: string[];
+
+// const them = ["0x78d42f467b225655ecc1cab6e19e0b901caae72c"];
+
+function getLpInfo(user) {
+  let totalWalletLpValue = BN_ZERO;
+
+  user.liquidityPositions.forEach((pos) => {
+    const lpPrice = pools[pos.pair.id]?.lpPrice || BN_ZERO;
+    const positionBalance = new BigNumber(pos.liquidityTokenBalance);
+    totalWalletLpValue = totalWalletLpValue.plus(positionBalance.multipliedBy(lpPrice));
+
+    // if (them.includes(user.id)) {
+    //   console.log(user.id);
+    //   console.log(pos.pair);
+    //   console.log(lpPrice.toString());
+    //   console.log(totalWalletLpValue.toString());
+    // }
+  });
+
+  return {
+    user: user.id,
+    totalWalletLpValue,
+  };
+}
+
+function getNftInfo(userInfo) {
+  const lpPrice = pools[userInfo.pool.lpToken].lpPrice;
+  const totalNftPoolBalance = new BigNumber(userInfo.balance);
+  const totalNftPoolValue = lpPrice.times(totalNftPoolBalance);
+
+  return {
+    pool: userInfo.pool.lpToken,
+    totalNftPoolBalance: totalNftPoolBalance.toString(),
+    totalNftPoolValue: totalNftPoolValue.toFixed(4),
+  };
+}
+
+async function getUserWalletLps(block: number) {
+  //  return readJSON(usersPath);
+
+  console.log("Fetching user wallet LP balances..");
+  const pageSize = 1000;
+  let skip = 0;
+  let moreToFetch = true;
+
+  let userData = [];
+
+  while (moreToFetch) {
+    const users: any = await modeV2Client.request(gql`
+      query UsersQuery {
+        users(
+          block: { number: ${block} },
+          first: ${pageSize},
+          skip: ${skip},
+          where: {
+            liquidityPositions_ : {
+              liquidityTokenBalance_gt: "0"
+            }
+          }) {
           id
-        }
-      }
-    `);
-
-    const nftPoolAddresses: string[] = pools.nftpools.map((p) => p.id);
-
-    // Aggregate any needed "pages" from subgraph for the block
-    // Max page size is 1000
-    const fileData = [];
-
-    const pageSize = 1000;
-    let skip = 0;
-    let moreToFetch = true;
-
-    while (moreToFetch) {
-      const [walletData, positionData]: [any, any] = await Promise.all([
-        modeV2Client.request(gql`
-        query UsersQuery {
-          users(block: { number: ${block} }, first: ${pageSize}, skip: ${skip}) {
-            id
-            liquidityPositions {
-              liquidityTokenBalance
-              pair {
+          liquidityPositions {
+            liquidityTokenBalance
+            pair {
+              id
+              reserveUSD
+              totalSupply
+              reserve0
+              reserve1
+              token0 {
                 id
-                reserveUSD
-                totalSupply
+                symbol
+              }
+              token1 {
+                id
+                symbol
               }
             }
           }
         }
-      `),
-        modeNFTPoolsClient.request(gql`
-        query UserPositionQuery {
-          userTotalBalanceForPools(block: { number: ${block} }, first: ${pageSize}, skip: ${skip}) {
-            balance
-            pool {
-              id
-              lpToken
-            }
-            user {
-              id
-            }
+      }`);
+
+    // If the max 1000 items are returned then there are more to fetch
+    moreToFetch = users.users.length === pageSize;
+    skip += pageSize;
+
+    userData = [...userData, ...users.users];
+    console.log(`${users.users.length} users`);
+
+    await sleepWaitPromise();
+  }
+
+  console.log(`${userData.length} total users`);
+  return userData;
+}
+
+async function getUserNFTPositionBalances(block: number) {
+  // return readJSON(userPositionsPath);
+  console.log("Fetching user NFTPool balances..");
+  const pageSize = 1000;
+  let skip = 0;
+  let moreToFetch = true;
+  let userData = [];
+
+  while (moreToFetch) {
+    const positionData: any = await modeNFTPoolsClient.request(gql`
+      query UserPositionQuery {
+        userTotalBalanceForPools(
+          block: { number: ${block} },
+          first: ${pageSize},
+          skip: ${skip},
+          where: { balance_gt: "0" }
+        ) {
+          balance
+          pool {
+            id
+            lpToken
+          }
+          user {
+            id
           }
         }
-    `),
-      ]);
+      }
+    `);
+    moreToFetch = positionData.userTotalBalanceForPools.length === pageSize;
+    skip += pageSize;
+    console.log(`${positionData.userTotalBalanceForPools.length} user nft balances`);
+    userData = [...userData, ...positionData.userTotalBalanceForPools];
 
-      // If the max 1000 items are returned then there are more to fetch
-      moreToFetch = walletData.users.length === pageSize;
-      skip += pageSize;
+    await sleepWaitPromise();
+  }
 
-      for (const user of walletData.users) {
-        const userAddress = user.id;
-        const userNftPoolPositions = positionData.userTotalBalanceForPools.filter((pos) => {
-          return pos.user?.id === userAddress;
-        });
+  console.log(`${userData.length} total user position balances`);
 
-        user.liquidityPositions.forEach((pos) => {
-          // Link the V2 pool with the NFTPool lpToken
-          const totalPositionsBalance =
-            userNftPoolPositions.find((p) => p.pool.lpToken === pos.pair.id)?.balance || "0";
+  return userData;
+}
 
-          const liquidityTokenBalance = new BigNumber(pos.liquidityTokenBalance).plus(
-            totalPositionsBalance
-          );
-          const reserveUSD = new BigNumber(pos.pair.reserveUSD);
-          const totalSupply = new BigNumber(pos.pair.totalSupply);
-          const lpPrice = reserveUSD.div(totalSupply);
+// To filter pool addresses out of snapshot list
+async function getNFTPoolAddresses() {
+  const pools: any = await modeNFTPoolsClient.request(gql`
+    query GetNFTPools {
+      nftpools {
+        id
+      }
+    }
+  `);
 
-          const lpvalue = lpPrice.isFinite()
-            ? liquidityTokenBalance.multipliedBy(lpPrice).toFixed(4)
-            : "0";
+  nftPoolAddresses = pools.nftpools.map((p) => p.id);
+}
 
-          // console.log("liquidityTokenBalance", liquidityTokenBalance.toString());
-          // console.log("totalPositionsBalance", totalPositionsBalance.toString());
-          // console.log("reserveUSD", reserveUSD.toString());
-          // console.log("lpPrice", lpPrice.toString());
-          // console.log("lpvalue", lpvalue);
+async function getBlockData(block: number) {
+  try {
+    const userWalletLps: any[] = await getUserWalletLps(block);
+    const positions: any[] = await getUserNFTPositionBalances(block);
 
-          if (
-            user.id !== "0x0000000000000000000000000000000000000000" &&
-            !nftPoolAddresses.includes(user.id)
-          ) {
-            fileData.push({
-              user: userAddress,
-              pool: pos.pair.id,
-              block,
-              lpvalue: commify(lpvalue),
-            });
-          }
-        });
+    let uniqueUserIds = [];
+
+    for (const user of userWalletLps) {
+      if (!uniqueUserIds.includes(user.id)) uniqueUserIds.push(user.id);
+
+      // Set LP prices for NFTPool only snapshots to use
+      user.liquidityPositions.forEach((lp) => {
+        const pair = lp.pair;
+        if (pools[pair.id]) return;
+
+        const reserveUSD = new BigNumber(pair.reserveUSD);
+        // if (reserveUSD.lt(1000)) continue;
+        const totalSupply = new BigNumber(pair.totalSupply);
+        const lpPrice = reserveUSD.div(totalSupply);
+
+        pools[pair.id] = {
+          lpPrice,
+          reserveUSD,
+          totalSupply,
+        };
+      });
+    }
+
+    for (const pos of positions) {
+      if (!uniqueUserIds.includes(pos.user.id)) uniqueUserIds.push(pos.user.id);
+    }
+
+    const hasBoth = [];
+    const lpOnly = [];
+    const nftOnly = [];
+
+    uniqueUserIds = uniqueUserIds.filter(
+      (id) =>
+        id !== "0x0000000000000000000000000000000000000000" &&
+        id !== "0x000000000000000000000000000000000000dead" &&
+        !nftPoolAddresses.includes(id)
+    );
+
+    for (const id of uniqueUserIds) {
+      const lpMatch = userWalletLps.find((u) => u.id === id);
+      const nftMatch = positions.find((p) => p.user.id === id);
+
+      if (lpMatch && nftMatch) {
+        hasBoth.push(id);
+        continue;
+      }
+
+      if (lpMatch) {
+        lpOnly.push(id);
+        continue;
+      }
+
+      if (nftMatch) {
+        nftOnly.push(id);
       }
     }
 
-    return fileData;
+    const withData = [];
+
+    for (const userId of hasBoth) {
+      const lpMatch = userWalletLps.find((u) => u.id === userId);
+      const nftMatch = positions.find((p) => p.user.id === userId);
+
+      const { totalWalletLpValue } = getLpInfo(lpMatch);
+      const { totalNftPoolBalance, totalNftPoolValue } = getNftInfo(nftMatch);
+
+      const totalLpValue = totalWalletLpValue.plus(totalNftPoolValue);
+
+      withData.push({
+        user: userId,
+        totalWalletLpValue: totalWalletLpValue.toFixed(4),
+        totalNftPoolBalance,
+        totalNftPoolValue,
+        totalLpValue: totalLpValue.toFixed(4),
+      });
+    }
+
+    for (const userId of lpOnly) {
+      const lpMatch = userWalletLps.find((u) => u.id === userId);
+      const { totalWalletLpValue } = getLpInfo(lpMatch);
+
+      withData.push({
+        user: userId,
+
+        totalWalletLpValue: totalWalletLpValue.toFixed(4),
+        totalNftPoolBalance: "0",
+        totalNftPoolValue: "0",
+        totalLpValue: totalWalletLpValue.toFixed(4),
+      });
+    }
+
+    for (const userId of nftOnly) {
+      const nftMatch = positions.find((p) => p.user.id === userId);
+      const { totalNftPoolBalance, totalNftPoolValue, pool } = getNftInfo(nftMatch);
+
+      withData.push({
+        user: userId,
+        pool,
+        totalNftPoolBalance,
+        totalNftPoolValue,
+        totalLpValue: totalNftPoolValue,
+      });
+    }
+
+    return withData.map((d) => {
+      return {
+        user: d.user,
+        pool: d.pool,
+        lpvalue: d.totalLpValue,
+        block,
+      };
+    });
   } catch (error) {
     console.error(error);
   }
@@ -167,8 +343,9 @@ async function writeCSV(
   }
 }
 
+
 // Goldsky rate limit of 50 requests per 10 seconds (= 5 per second = 200ms delay. Using buffer with 300ms)
-export function sleepWaitPromise(milliseconds = 100, log = true) {
+export function sleepWaitPromise(milliseconds = 500, log = true) {
   if (log) {
     console.log(`sleepWaitPromise: waiting delay of ${milliseconds} ms...`);
   }
@@ -178,9 +355,11 @@ export function sleepWaitPromise(milliseconds = 100, log = true) {
 
 async function run() {
   try {
-    const nftPoolFactoryStartBlock = 3359145; // block first pool was created (subgraph first data block says 3_326_752)
-    const v2FactoryBlock = 3326753; // V2 factory first pair creation block 3326753
-    // Each block ~1 hour after the previous
+    await getNFTPoolAddresses();
+
+    // const testBlock = 6599713; // 4/17 11:17am EST
+    // const fileData = await getBlockData(testBlock);
+
     const csvFilePath = path.resolve(__dirname, '../../../../data/mode_swapmodev2_hourly_blocks.csv');
     const hourlyBlocks = await readBlocksFromCSV(csvFilePath);
 
@@ -192,7 +371,6 @@ async function run() {
       fileData = [...fileData, ...data];
     }
 
-        // Write the CSV output to a file
     const csvOutPath = path.resolve(__dirname, "../../../../data/mode_swapmodev2_tvl_snapshot.csv");
     const ws = fs.createWriteStream(csvOutPath);
     await writeCSV(
