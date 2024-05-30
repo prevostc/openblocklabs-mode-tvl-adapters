b07d370daaae3abd68cd83c045d66d4d645fe015
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-05-30 19:50:22 +0200

    Use goldsky tags

--- adapters/tvl-snapshot-beefy/src/config.ts
+++ adapters/tvl-snapshot-beefy/src/config.ts
@@ -2,9 +2,7 @@ export const BEEFY_VAULT_API = "https://api.beefy.finance/vaults";
 
 // subgraph source: https://github.com/beefyfinance/l2-lxp-liquidity-subgraph
 export const BEEFY_SUBGRAPH_URL =
-  "https://api.goldsky.com/api/public/project_clu2walwem1qm01w40v3yhw1f/subgraphs/beefyfinance/l2-lxp-liquidity-mode/gn";
-//export const BEEFY_SUBGRAPH_URL =
-//  "https://api.0xgraph.xyz/subgraphs/name/beefyfinance/l2-lxp-liquidity-mode";
+  "https://api.goldsky.com/api/public/project_clu2walwem1qm01w40v3yhw1f/subgraphs/beefy-l2-lxp-liquidity-mode/latest/gn";
 
 export const SUBGRAPH_PAGE_SIZE = 1000;
 
