c461acfe2a0e5df4e0bfa6d5047e6bb0c0c8d00b
PRINT3Rxyz <brrr@print3r.xyz>
2024-05-30 11:57:39 +0400

    Final Push

--- adapters/tvl-snapshot-print3r/src/sdk/subgraphDetails.ts
+++ adapters/tvl-snapshot-print3r/src/sdk/subgraphDetails.ts
@@ -1,45 +1,43 @@
 import { CHAINS, PROTOCOLS, SUBGRAPH_URLS } from "./config";
 import { Position } from "../types";
 
-export const getLiquidityProviders = async (block: number) => {
-  let subgraphUrl = SUBGRAPH_URLS[CHAINS.MODE][PROTOCOLS.PRINT3R][0];
+export const getLiquidityProviders = async (
+  block: number
+): Promise<Position[]> => {
+  const subgraphUrl = SUBGRAPH_URLS[CHAINS.MODE][PROTOCOLS.PRINT3R][0];
 
   let skip = 0;
   let fetchNext = true;
-  let liquidityProviders: Position[] = [];
-  while (fetchNext) {
-    const query = `{
-      brrrStats(
-        where: { period: total, mintAmount_gt: "0", account_not: "0x0000000000000000000000000000000000000000" }
-        first: 1000
-        skip: ${skip}
-      ) {
-        blockNumber
-        account
-        mintAmount
-      }
-    }`;
+  const liquidityProviders: Position[] = [];
 
-    let response = await fetch(subgraphUrl, {
+  while (fetchNext) {
+    const query = `
+      {
+        stakedBrrrBalances(
+          where: { balance_gt: "0", blockNumber_lte: "${block}" }
+          first: 1000
+          skip: ${skip}
+      ) {
+          user
+          balance
+          blockNumber
+        }
+      }
+    `;
+
+    const response = await fetch(subgraphUrl, {
       method: "POST",
       body: JSON.stringify({ query }),
       headers: { "Content-Type": "application/json" },
     });
 
-    let data: any = await response.json();
+    const data = await response.json();
+    const fetchedProviders = data.data.stakedBrrrBalances || [];
 
-    const fetchedProviders = data.data.brrrStats || [];
-
-    for (let provider of fetchedProviders) {
-      console.log(
-        "Provider Block Number:",
-        provider.blockNumber,
-        "Type:",
-        typeof provider.blockNumber
-      );
-      let transformedPosition: Position = {
-        account: provider.account,
-        amount: BigInt(provider.mintAmount),
+    for (const provider of fetchedProviders) {
+      const transformedPosition: Position = {
+        account: provider.user,
+        amount: BigInt(provider.balance),
         blockNumber: parseInt(provider.blockNumber), // Convert to number
       };
       liquidityProviders.push(transformedPosition);
@@ -52,14 +50,5 @@ export const getLiquidityProviders = async (block: number) => {
     }
   }
 
-  console.log("Filtering by Block:", block, "Type:", typeof block);
-
-  // Filter the results to only include positions with the specified block number
-  const filteredProviders = liquidityProviders.filter(
-    (provider) => provider.blockNumber === block
-  );
-
-  console.log("Filtered Providers:", filteredProviders);
-
-  return filteredProviders;
+  return liquidityProviders;
 };
